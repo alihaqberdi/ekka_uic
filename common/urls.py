6c471c6634b73507262a6f7a2e33cd77d2b29860
a.haqberdiyev <a.haqberdiyev@uic.group>
2024-02-26 22:39:18 +0500

    feat: add common section

--- common/urls.py
+++ common/urls.py
@@ -1,6 +1,9 @@
-from common.views import IndexView
+from common.views import IndexView, search_view
 from django.urls import path
+
+app_name = "common"
 
 urlpatterns = [
     path('', IndexView.as_view(), name="index"),
+    path('SearchView/', search_view, name='search')
 ]
--- common/views.py
+++ common/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.db.models import Max, Min, Avg, Count, Q
 from django.views import View
-from product.models import Product
+from product.models import Product, Size, Category, Color, Price
+from django.contrib.postgres.search import TrigramSimilarity
+
+
+# trigram extension
 
 
 # Create your views here.
@@ -11,4 +16,18 @@ class IndexView(TemplateView):
     template_name = 'index.html'
 
 
+def search_view(request):
+    q = request.POST.get("q")
+    products = Category.objects.all()
+    if q:
+        products = products.annotate(
+            similarity=TrigramSimilarity('title', q),
+        ).oreder_by('-similarity')
 
+    context = {
+        "product_ls": products,
+        "size_ls": Size.objects.all(),
+        "category_ls": Category.objects.all(),
+        "color": Color.objects.all()
+    }
+    return render(request, "product_list.html", context)
