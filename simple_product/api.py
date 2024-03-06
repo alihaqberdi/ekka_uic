7b778bc8981906cbb13ccf8fb47e6e05a6a0a99b
alihaqberdi <alhppypro@gmail.com>
2024-03-06 21:01:33 +0500

    feat: api integration

--- simple_product/api.py
+++ simple_product/api.py
@@ -1,27 +1,73 @@
 from rest_framework.views import APIView
-from simple_product.models import Product
+from simple_product.models import Product, Category, ProductImage
+from rest_framework.generics import ListAPIView, RetrieveAPIView, DestroyAPIView
 from rest_framework.response import Response
 from rest_framework import serializers
 
 
+class ProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImage
+        fields = (
+            "id",
+            "product",
+            "image"
+        )
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = (
+            "id",
+            "title",
+            "parent"
+        )
+
+
 class ProductListAPIViewSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+    images = ProductImageSerializer(many=True)
+    total_amount = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
         fields = (
+            "id",
             "category",
             "title",
             "slug",
             "text",
             "main_image",
             "price",
-            "count"
+            "count",
+            "images",
+            "total_amount"
         )
 
+    def get_total_amount(self, vals):
+        return vals.price * vals.count
 
-class ProductListAPIView(APIView):
-    def get(self, request, *args, **kwargs):
-        data = Product.objects.first()
 
-        serializer = ProductListAPIViewSerializer(data)
+class ProductListAPIView(ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductListAPIViewSerializer
 
-        return Response(serializer.data, status=200)
+
+class ProductRetrieveAPIView(RetrieveAPIView):
+    serializer_class = ProductListAPIViewSerializer
+    queryset = Product.objects.all()
+    lookup_field = "pk"
+
+
+class ProductDestroyAPIView(DestroyAPIView):
+    serializer_class = ProductListAPIViewSerializer
+    queryset = Product.objects.all()
+
+
+# def get(self, request, *args, **kwargs):
+#     data = Product.objects.all()
+#
+#     serializer = ProductListAPIViewSerializer(data, many=True)
+#
+#     return Response(serializer.data, status=200)
--- simple_product/urls.py
+++ simple_product/urls.py
@@ -1,15 +1,17 @@
 from simple_product.views import ShopListView, product_detail
 from django.urls import path
-from simple_product.api import ProductListAPIView
+from simple_product.api import ProductListAPIView, ProductRetrieveAPIView, ProductDestroyAPIView
 
 app_name = 'product'
 
 urlpatterns = [
     path('ProductList/', ShopListView.as_view(), name="shop_list"),
-    path("ProductDetail/<slug:slug>/", product_detail, name='detail'),
+    # path("ProductDetail/<slug:slug>/", product_detail, name='detail'),
 ]
 
 """API URL"""
 urlpatterns += [
-    path("ProductListApiView/",ProductListAPIView.as_view())
+    path("ProductListApiView/",ProductListAPIView.as_view()),
+    path("ProductDetail/<int:pk>/", ProductRetrieveAPIView.as_view()),
+    path("ProductDestroy/<int:pk>/", ProductDestroyAPIView.as_view())
 ]
